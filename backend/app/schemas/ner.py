1981b70ca322a9bd6b6cedabb7a88e6e0559c86a
vincent porte <vincent@neuralia.co>
2022-01-04 09:17:57 +0100

    default ner test sentence

--- backend/app/schemas/ner.py
+++ backend/app/schemas/ner.py
@@ -2,8 +2,10 @@ from pydantic import BaseModel
 from typing import Optional
 from typing import List, Dict, Tuple
 
+
 class NerQuery(BaseModel):
-    sentence: str
+    sentence: str = "urgent, devis pour 6000 flyers a5, quadri rv, cdm, sous film par paquet de 100, livraison 72, merci"
+
 
 class NerEntity(BaseModel):
     text: str
@@ -11,6 +13,7 @@ class NerEntity(BaseModel):
     pos: int
     start: int
     end: int
+
 
 class NerEntities(BaseModel):
     entities: List[NerEntity]
